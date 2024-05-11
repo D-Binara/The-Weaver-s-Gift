4aeb815fed35268f8d06562ed19e76b68b57500a
D. Binara Kaveesha Weerasekara <dbkaveesha@gmail.com>
2024-05-11 23:54:20 +0530

    add tailwind css

--- The-Weaver-s-Gift/src/App.tsx
+++ The-Weaver-s-Gift/src/App.tsx
@@ -1,13 +1,20 @@
-import './App.css'
+import "./App.css";
 import ListGroup from "./components/ListGroup";
-
+import Navbar from "./components/nav_bar";
 
 function App() {
+  return (
+    <div>
+       <Navbar></Navbar>
+      <h1 className="">Counter</h1>
+      <p>Count: </p>
 
-    return
+      <ListGroup>
 
+      </ListGroup>
+    </div>
 
-
+    
+  );
 }
-
-export default App
+export default App;
---
+++ The-Weaver-s-Gift/src/components/nav_bar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>contact Us</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
