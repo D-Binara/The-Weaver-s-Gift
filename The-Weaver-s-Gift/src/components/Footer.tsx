42339b8e44a6a7232a2ba4405a08131824737995
D-Binara <dbkaveesha@gmail.com>
2024-09-13 23:58:27 +0530

    Change the images and add Story css

--- The-Weaver-s-Gift/src/components/Footer.tsx
+++ The-Weaver-s-Gift/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-blue-900 text-white p-4">
+    <footer className="bg-slate-800 text-white p-4">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 Ancient Mesopotamian Myth. All rights reserved.</p>
       </div>
--- The-Weaver-s-Gift/src/pages/Gods.tsx
+++ The-Weaver-s-Gift/src/pages/Gods.tsx
@@ -13,9 +13,9 @@ const Gods: React.FC = () => {
         />
         {/* Repeat the GodCard component for other gods as needed */}
         <GodCard
-          name="Enki"
+          name="Nammu - The Mother of All"
           image={enkiImage}
-          description="Enki is the Sumerian god of water, knowledge, mischief, crafts, and creation. He is often depicted with flowing water and fish."
+          description="Nammu, the great goddess of creation, floated upon the waters of the primordial realm..."
         />
         <GodCard
           name="Enki"
