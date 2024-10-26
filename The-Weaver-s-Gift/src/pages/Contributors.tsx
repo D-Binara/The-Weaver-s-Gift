4693c570044a9ebcbe7f39d4b2aa94a3c619badf
D-Binara <dbkaveesha@gmail.com>
2024-10-26 11:39:47 +0530

    Update contributors

--- The-Weaver-s-Gift/src/pages/Contributors.tsx
+++ The-Weaver-s-Gift/src/pages/Contributors.tsx
@@ -7,6 +7,7 @@ const Contributors: React.FC = () => {
       <div className=" flex justify-center gap-10 py-10 flex-wrap">
         <GitHubUserCard username="D-Binara" />
         <GitHubUserCard username="Matheus-Parreira" />
+          <GitHubUserCard username="ChaudaryHammad" />
       </div>
     </main>
   );
